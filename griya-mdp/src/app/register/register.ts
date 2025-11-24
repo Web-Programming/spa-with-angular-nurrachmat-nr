ac105cc1e04964fc0b914445306871641e7240ca
Nur Rachmat <rachmat.nur91@gmail.com>
2025-11-24 12:17:07 +0700

    Implement login and register forms with validation

--- griya-mdp/src/app/register/register.ts
+++ griya-mdp/src/app/register/register.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  registerForm: FormGroup;
+  showPassword = false;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  submitRegister(): void {
+    if (this.registerForm.valid) {
+      const formData = this.registerForm.value;
+
+      console.log('Form submitted', formData);
+
+      // TODO: Kirim data ke backend API
+      // this.authService.register(formData).subscribe(...)
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
